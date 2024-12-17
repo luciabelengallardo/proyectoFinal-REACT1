3f516bf438477234f485363ff022d7785718a627
Lucia Gallardo <luciagallardoherrera@gmail.com>
2024-12-17 02:02:12 -0300

    creando componente home

---
+++ proyectoFinal-React-Grupo1/src/pages/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function HomeScreen() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2>Home Screen</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomeScreen;
