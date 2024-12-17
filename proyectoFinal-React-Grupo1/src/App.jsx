aea0a9ef24e28205846584fa36a6adcc80f5e0f5
Lucia Gallardo <luciagallardoherrera@gmail.com>
2024-12-17 02:03:27 -0300

    importando componentes a app

--- proyectoFinal-React-Grupo1/src/App.jsx
+++ proyectoFinal-React-Grupo1/src/App.jsx
@@ -1,16 +1,29 @@
 // import "./App.css";
-import Footer from "./components/Footer";
+import "./css/home.css";
+import "./css/footer.css";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeScreen from "./pages/HomeScreen";
+import ErrorScreen from "./pages/ErrorScreen";
+import AdminScreen from "./pages/AdminScreen";
+import LoginScreen from "./pages/LoginScreen";
+import AboutUsScreen from "./pages/AboutUsScreen";
 
 function App() {
   return (
     <>
-      <div>
+      <BrowserRouter>
         <NavBar />
-      </div>
-      <div>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/AboutUs" element={<AboutUsScreen />} />
+          <Route path="/*" element={<ErrorScreen />} />
+          <Route path="/Login" element={<LoginScreen />} />
+          <Route path="/Admin" element={<AdminScreen />} />
+        </Routes>
         <Footer />
-      </div>
+      </BrowserRouter>
     </>
   );
 }
