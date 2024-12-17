b67808f050e05d84785db3b3e73c0a93b5e07b3c
Lucia Gallardo <luciagallardoherrera@gmail.com>
2024-12-17 02:01:40 -0300

    creando componente error

---
+++ proyectoFinal-React-Grupo1/src/pages/ErrorScreen.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ErrorScreen() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2>Error 404 Screen</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ErrorScreen;
