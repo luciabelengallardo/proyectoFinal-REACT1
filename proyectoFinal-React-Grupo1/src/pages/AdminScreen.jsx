1d193821e86a89a80b080b382a7342ee93bb0c7f
Lucia Gallardo <luciagallardoherrera@gmail.com>
2024-12-17 02:01:05 -0300

    creando componente admin

---
+++ proyectoFinal-React-Grupo1/src/pages/AdminScreen.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function AdminScreen() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2>Admin Screen</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AdminScreen;
