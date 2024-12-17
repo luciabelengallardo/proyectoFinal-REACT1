0a083f6d88c109a44fa6f4d144fc02f5ab739c4a
Lucia Gallardo <luciagallardoherrera@gmail.com>
2024-12-17 01:41:08 -0300

    cambios menores

--- proyectoFinal-React-Grupo1/src/App.jsx
+++ proyectoFinal-React-Grupo1/src/App.jsx
@@ -1,29 +1,16 @@
 // import "./App.css";
-import "./css/home.css";
-import "./css/footer.css";
-import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomeScreen from "./pages/HomeScreen";
-import ErrorScreen from "./pages/ErrorScreen";
-import AdminScreen from "./pages/AdminScreen";
-import LoginScreen from "./pages/LoginScreen";
-import AboutUsScreen from "./pages/AboutUsScreen";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <div>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/AboutUs" element={<AboutUsScreen />} />
-          <Route path="/*" element={<ErrorScreen />} />
-          <Route path="/Login" element={<LoginScreen />} />
-          <Route path="/Admin" element={<AdminScreen />} />
-        </Routes>
+      </div>
+      <div>
         <Footer />
-      </BrowserRouter>
+      </div>
     </>
   );
 }
