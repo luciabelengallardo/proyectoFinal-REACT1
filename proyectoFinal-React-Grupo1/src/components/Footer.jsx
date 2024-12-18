5c6d5bb78a8fb0bcb883f4bf705ab5ec8b09ca49
Lucia Gallardo <luciagallardoherrera@gmail.com>
2024-12-18 15:59:01 -0300

    agregando link redes en footer

--- proyectoFinal-React-Grupo1/src/components/Footer.jsx
+++ proyectoFinal-React-Grupo1/src/components/Footer.jsx
@@ -9,36 +9,44 @@ function Footer() {
       <div className="footer_contenedor d-flex flex-column align-items-stretch">
         <div className="row d-flex justify-content-between">
           <div className="col-sm-12 col-md-4">
-            <Link className="" to="/">
+            <Link className="navbar-brand" to="/">
               <img src={logo} alt="logo-movie-night" />
             </Link>
           </div>
           <div className="col-sm-12 col-md-4 d-flex flex-column align-items-center">
-            <h5>Soporte</h5>
+            {/* <h5>Soporte</h5> */}
             <ul className="d-flex justify-content-center flex-md-column">
-              <li className="pe-2">Quienes Somos</li>
-              <li className="ps-2 ps-md-0">Ayuda</li>
+              <li className="pe-2 ">
+                <Link className="text-white footer-link" to="/AboutUs">
+                  Quienes Somos
+                </Link>
+              </li>
+              <li className="ps-2 ps-md-0 ">
+                <Link className="text-white footer-link" to="/ayuda">
+                  Ayuda
+                </Link>
+              </li>
             </ul>
           </div>
 
           {/* <!-- Redes --> */}
           <div className="col-sm-12 col-md-4 d-flex flex-column align-items-center footer_redes">
             <div className="icons">
-              <a href="">
+              <Link className="text-white" to="/facebook">
                 <i className="bi bi-facebook p-1"></i>
-              </a>
-              <a href="">
+              </Link>
+              <Link className="text-white" to="/instagram">
                 <i className="bi bi-instagram p-1"></i>
-              </a>
-              <a href="">
+              </Link>
+              <Link className="text-white" to="/google">
                 <i className="bi bi-google p-1"></i>
-              </a>
-              <a href="">
+              </Link>
+              <Link className="text-white" to="/whatsapp">
                 <i className="bi bi-whatsapp p-1"></i>
-              </a>
-              <a href="">
+              </Link>
+              <Link className="text-white" to="/twitch">
                 <i className="bi bi-twitch p-1"></i>
-              </a>
+              </Link>
             </div>
           </div>
         </div>
