00cc9ecfa5831908e448245b564f43aca5223604
Lucia Gallardo <luciagallardoherrera@gmail.com>
2024-12-21 20:52:32 -0300

    cambios en login screen

--- proyectoFinal-React-Grupo1/src/App.jsx
+++ proyectoFinal-React-Grupo1/src/App.jsx
@@ -9,18 +9,24 @@ import ErrorScreen from "./pages/ErrorScreen";
 import LoginScreen from "./pages/LoginScreen";
 import AboutUsScreen from "./pages/AboutUsScreen";
 import AdminMovies from "./pages/Adminmovies";
+import { useState } from "react";
 
 function App() {
+  const [usuario, setUsuario] = useState();
+
   return (
     <>
       <BrowserRouter>
-        <NavBar />
+        <NavBar usuario={usuario} setUsuario={setUsuario} />
 
         <Routes>
           <Route path="/" element={<HomeScreen />} />
           <Route path="/AboutUs" element={<AboutUsScreen />} />
           <Route path="/*" element={<ErrorScreen />} />
-          <Route path="/Login" element={<LoginScreen />} />
+          <Route
+            path="/Login"
+            element={<LoginScreen usuario={usuario} setUsuario={setUsuario} />}
+          />
           <Route path="/Admin" element={<AdminMovies />} />
         </Routes>
         <Footer />
