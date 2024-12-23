7d258a989c57f43fb64245e5bd9fcb0b51d5dfd6
Lucia Gallardo <luciagallardoherrera@gmail.com>
2024-12-23 13:36:12 -0300

    about us estilos

--- proyectoFinal-React-Grupo1/src/pages/AboutUsScreen.jsx
+++ proyectoFinal-React-Grupo1/src/pages/AboutUsScreen.jsx
@@ -9,30 +9,41 @@ const AboutUs = () => {
       name: "Lucia",
       description:
         "Mi nombre es Lucia Gallardo, soy estudiante de Programacion Web en Rolling Code School. Me gusta viajar y aprender del mundo de la tecnologia",
-      image: "../img/lucia.jpg",
+      image: Lucia,
     },
     {
       name: "Gonzalo",
       description:
         "Hola! mi nombre es Gonzalo. Estudio programación web en rolling code, y soy técnico en una empresa de sistemas de comunicación. Hincha y socio del Club Atletico Boca Juniors",
-      image: "../img/gonzalo.jpg",
+      image: Gonzalo,
     },
     {
       name: "Ignacio",
-      description: ".",
-      image: "../img/nacho.jpg",
+      description:
+        "Hola, mi nombre es Ignacio Gonzalez Dupuy, tengo 21 años. Estoy estudiando programacion y soy futbolista amateur",
+      image: Ignacio,
     },
   ];
 
   return (
-    <div className="about-us">
-      <h1>Sobre Nosotros</h1>
-      <div className="team-container">
+    <div className=" container py-5">
+      <h1 className="text-center text-white mb-4">Sobre Nosotros</h1>
+      <div className="row justify-content-center">
         {teamMembers.map((member, index) => (
-          <div className="team-member" key={index}>
-            <img src={member.image} alt={member.name} className="team-image" />
-            <h2>{member.name}</h2>
-            <p>{member.description}</p>
+          <div className="col-md-4 mb-4 container-card" key={index}>
+            <div className="card">
+              <div className="card-tamano">
+                <img
+                  src={member.image}
+                  alt={member.name}
+                  className="card-img"
+                />
+              </div>
+              <div className="card-body">
+                <h5 className="card-title text-center">{member.name}</h5>
+                <p className="card-text">{member.description}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
