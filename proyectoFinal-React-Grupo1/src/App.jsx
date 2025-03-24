30d8968fffa516b92bfc0a61c8757ce9b33f727d
Lucia Gallardo <luciagallardoherrera@gmail.com>
2025-03-24 12:49:51 -0300

    agregando ruta movie provider en app

--- proyectoFinal-React-Grupo1/src/App.jsx
+++ proyectoFinal-React-Grupo1/src/App.jsx
@@ -13,48 +13,51 @@ import AdminMovies from "./pages/Adminmovies";
 import { useState } from "react";
 import RegisterScreen from "./pages/RegisterScreen";
 import ContactoScreen from "./pages/ContactoScreen";
+import { MovieProvider } from "./context/MovieContext";
 
 function App() {
   const [usuario, setUsuario] = useState();
 
   return (
     <>
-      <BrowserRouter>
-        <NavBar usuario={usuario} setUsuario={setUsuario} />
+      <MovieProvider>
+        <BrowserRouter>
+          <NavBar usuario={usuario} setUsuario={setUsuario} />
 
-        <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/AboutUs" element={<AboutUsScreen />} />
-            <Route path="/*" element={<ErrorScreen />} />
-            <Route
-              path="/Login"
-              element={
-                <LoginScreen usuario={usuario} setUsuario={setUsuario} />
-              }
-            />
-            <Route path="/Contacto" element={<ContactoScreen />} />
-
-            <Route path="/register" element={<RegisterScreen />} />
-            {usuario && usuario.rol == "admin" && (
-              <Route path="/Admin" element={<AdminMovies />} />
-            )}
-
-            <Route
-              path="/Movies/:id"
-              element={
-                usuario ? (
-                  <MoviesCategory />
-                ) : (
+          <main>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/AboutUs" element={<AboutUsScreen />} />
+              <Route path="/*" element={<ErrorScreen />} />
+              <Route
+                path="/Login"
+                element={
                   <LoginScreen usuario={usuario} setUsuario={setUsuario} />
-                )
-              }
-            />
-          </Routes>
-        </main>
+                }
+              />
+              <Route path="/Contacto" element={<ContactoScreen />} />
 
-        <Footer />
-      </BrowserRouter>
+              <Route path="/register" element={<RegisterScreen />} />
+              {usuario && usuario.rol == "admin" && (
+                <Route path="/Admin" element={<AdminMovies />} />
+              )}
+
+              <Route
+                path="/Movies/:id"
+                element={
+                  usuario ? (
+                    <MoviesCategory />
+                  ) : (
+                    <LoginScreen usuario={usuario} setUsuario={setUsuario} />
+                  )
+                }
+              />
+            </Routes>
+          </main>
+
+          <Footer />
+        </BrowserRouter>
+      </MovieProvider>
     </>
   );
 }
